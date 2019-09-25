2419f48827cf6c6a4d6668d6d99c1b500d5cd329
Redwan Hussain <hussain.redwan10@gmail.com>
2019-09-25 16:23:12 +0600

    page design done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,10 @@
 (function($){
     "use strict";
-    // Mobile menu
-    jQuery('#mobile-menu-active').meanmenu({
-        meanMenuContainer: '.mobile-menu',
-        meanScreenWidth: "991"
+
+
+    $(document).ready(function() {
+      $('#categorey').niceSelect();
     });
-
-
-
 
 
 })(jQuery);
@@ -18,32 +15,18 @@
 
 
 
-//  var acc = document.getElementsByClassName("dropdown");
-//  var i;
-//  for (i = 0; i < acc.length; i++) {
-//    acc[i].addEventListener("click", function() {
-//      this.classList.toggle("active");
-//      var panel = this.nextElementSibling;
-//      if (panel.style.display === "block") {
-//        panel.style.display = "none";
-//      } else {
-//        panel.style.display = "block";
-//      };
-//    });
-//  }
 
+// window.onscroll = function() {myFunction()};
 
-window.onscroll = function() {myFunction()};
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 
 
